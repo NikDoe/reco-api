b90eb92b1a21b0aad0685bdd87a4ae66d2346e9c
NikDoe <nikd0e@yandex.ru>
2023-07-30 16:44:49 +0300

    fix: update user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from './user.controller';
 
 @Module({
     imports: [PrismaModule],
-    providers: [AuthService],
-    controllers: [AuthController, UserController],
+    providers: [],
+    controllers: [UserController],
 })
 export class UserModule {}
