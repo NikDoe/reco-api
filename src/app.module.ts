fe9b806b5a95f8ff3496e342114fbcfe319c6702
NikDoe <nikd0e@yandex.ru>
2023-07-30 17:00:50 +0300

    feat: add config for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [PrismaModule, UserModule],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
+        PrismaModule,
+        UserModule
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
