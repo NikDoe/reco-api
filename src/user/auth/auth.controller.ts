b06dcaa6c46f274f7f56b7b5dd2e14abca08e166
NikDoe <nikd0e@yandex.ru>
2023-07-31 19:17:58 +0300

    fix: make signup & signin routes public

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Param, ParseEnumPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserType } from '@prisma/client';
 import { SigninDto, SignupDto } from '../dto/auth.dto';
+import { Public } from './decorators';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @Public()
     @Post('/signup/:userType')
     async signup(
         @Body() body: SignupDto,
@@ -15,6 +17,7 @@ export class AuthController {
         return await this.authService.signup(body, userType);
     }
 
+    @Public()
     @Post('/signin')
     async signin(@Body() body: SigninDto) {
         return this.authService.signin(body);
